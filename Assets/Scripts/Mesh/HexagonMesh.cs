e4e8a5b00f875aeed6d224f3d7e6bafde992293c
killersshadow <killersshadow@gmail.com>
2019-09-23 22:09:18 +0200

    square

--- Assets/Scripts/Mesh/HexagonMesh.cs
+++ Assets/Scripts/Mesh/HexagonMesh.cs
@@ -7,6 +7,9 @@ public class HexagonMesh : MonoBehaviour
 {
     Mesh mesh;
 
+    Vector3[] vertices;
+    int[] triangles;
+
     void Awake()
     {
         mesh = GetComponent<MeshFilter>().mesh;
@@ -14,7 +17,22 @@ public class HexagonMesh : MonoBehaviour
 
     void Start()
     {
+        CreateMesh();
+        DrawMesh();
+    }
 
+    void CreateMesh()
+    {
+        vertices = new Vector3[] { new Vector3(0, 0, 0), new Vector3(0, 0, 1), new Vector3(1, 0, 0), new Vector3(1, 0, 1),
+        };
+        triangles = new int[] {0,1,2,2,1,3};
+    }
+
+    void DrawMesh()
+    {
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
     }
 
 
