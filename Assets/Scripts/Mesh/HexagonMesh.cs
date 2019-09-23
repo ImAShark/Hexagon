f82c4e78dc63016fa091264fe0d538b3bf2c1059
killersshadow <32833605+killersshadow@users.noreply.github.com>
2019-09-23 11:42:48 +0200

    start

---
+++ Assets/Scripts/Mesh/HexagonMesh.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class HexagonMesh : MonoBehaviour
+{
+    Mesh mesh;
+
+    void Awake()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+    }
+
+    void Start()
+    {
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //https://www.youtube.com/watch?v=yGfS-U740x4
+}
+
