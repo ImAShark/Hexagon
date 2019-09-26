d65cea122392c2ab425c08d14066c8ce4a5d907c
killersshadow <32833605+killersshadow@users.noreply.github.com>
2019-09-26 14:11:07 +0200

    created hexagon mesh

--- Assets/Scripts/Mesh/HexagonMesh.cs
+++ Assets/Scripts/Mesh/HexagonMesh.cs
@@ -23,9 +23,24 @@ public class HexagonMesh : MonoBehaviour
 
     void CreateMesh()
     {
-        vertices = new Vector3[] { new Vector3(0, 0, 0), new Vector3(0, 0, 1), new Vector3(1, 0, 0), new Vector3(1, 0, 1),
-        };
-        triangles = new int[] {0,1,2,2,1,3};
+        vertices = new Vector3[] {
+          new Vector3(0, 0, 0),     //0
+          new Vector3(1, 0, 0.5f),  //1
+          new Vector3(1, 0, 0),     //2
+          new Vector3(2, 0, 0),     //3
+          new Vector3(2, 0, -1),    //4
+          new Vector3(1, 0, -1),    //5
+          new Vector3(1, 0, -1.5f), //6
+          new Vector3(0, 0, -1),    //7
+         }; 
+        triangles = new int[] {0,1,2,
+                               2,1,3,
+                               2,3,4,
+                               5,2,4,
+                               6,5,4,
+                               7,5,6,
+                               7,0,2,
+                               7,2,5 };
     }
 
     void DrawMesh()
